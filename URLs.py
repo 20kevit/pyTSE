6d2418228e9f54653b8cfca3a5f842ceae6ee611
20kevit <o.h.e.1380@gmail.com>
2023-10-23 18:29:12 +0330

    URLs file created

---
+++ URLs.py
@@ -0,0 +1,5 @@
+
+LAST_POSSIBLE_DAY = "http://service.tsetmc.com/tsev2/data/TseClient2.aspx?t=LastPossibleDeven"
+TICKER_HISTORY = "http://old.tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
+GROUP_TICKER = "http://old.tsetmc.com/tsev2/data/InstValue.aspx?g={}&t=g&s={}"
+INDEX_COMPANY = "http://cdn.tsetmc.com/api/ClosingPrice/GetIndexCompany/{}"
