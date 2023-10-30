99a9b44069a6554b4ebb7112f0a65dc190b7c32b
20kevit <o.h.e.1380@gmail.com>
2023-10-30 19:58:30 +0330

    values should be integer

--- tools.py
+++ tools.py
@@ -3,15 +3,15 @@ from datetime import date, time
 
 def dEven_to_date(dEven: int | str):
     dEven = str(dEven)
-    year = dEven[:4]
-    month = dEven[4:-2]
-    day = dEven[-2:]
+    year = int(dEven[:4])
+    month = int(dEven[4:-2])
+    day = int(dEven[-2:])
     return date(year, month, day)
 
 
 def hEven_to_time(hEven: int | str):
     hEven = str(hEven)
-    hour = hEven[:2]
-    minute = hEven[2:-2]
-    second = hEven[-2:]
+    hour = int(hEven[:2])
+    minute = int(hEven[2:-2])
+    second = int(hEven[-2:])
     return time(hour, minute, second)
